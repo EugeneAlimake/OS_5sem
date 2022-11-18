965df7229797ecb75d75609f3648eb20ce2f6ce2
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2022-11-18 10:46:36 +0300

    Add files via upload

---
+++ 5 лабораторная работа/os05_01/os05_01/os05_01.cpp
@@ -0,0 +1,85 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <bitset>
+
+void printProcessPrty(HANDLE h)
+{
+    DWORD prty = GetPriorityClass(h); //приоритет процесса
+    //DWORD — 32-битное беззнаковое целое.
+    std::cout << " ----- Current PID = " << GetCurrentProcessId() << " \n";
+    switch (prty)
+    {
+    case IDLE_PRIORITY_CLASS:         std::cout << " ----+ priority = IDLE_PRIORITY_CLASS \n"; break;
+    case BELOW_NORMAL_PRIORITY_CLASS: std::cout << " ----+ priority = BELOW_NORMAL_PRIORITY_CLASS \n"; break;
+    case NORMAL_PRIORITY_CLASS:       std::cout << " ----+ priority = NORMAL_PRIORITY_CLASS \n"; break;
+    case ABOVE_NORMAL_PRIORITY_CLASS: std::cout << " ----+ priority = ABOVE_NORMAL_PRIORITY_CLASS \n"; break;
+    case HIGH_PRIORITY_CLASS:         std::cout << " ----+ priority = HIGH_PRIORITY_CLASS \n"; break;
+    case REALTIME_PRIORITY_CLASS:     std::cout << " ----+ priority = REALTIME_PRIORITY_CLASS \n"; break;
+    default:                          std::cout << " ----+ priority = ? \n"; break;
+    }
+    return;
+}
+
+void printThreadPrty(HANDLE h)//приоритет потока
+{
+    DWORD icpu = SetThreadIdealProcessor(h // дескриптор потока
+        , MAXIMUM_PROCESSORS// номер идеального процессора
+    );//устанавливает предпочитаемый процессор для потока
+        DWORD prty = GetThreadPriority(h);
+        std::cout << " ---- Current Thread ID = " << GetCurrentThreadId() << "\n";
+    // std::cout << " ----+ Processor = " << GetCurrentProcessorNumber() << " \n";//Функция возвращает текущий номер процессора.
+        std::cout << " ----+ Processor = " << icpu << " \n";
+        std::cout << " ----+ MAXProcessors = " << MAXIMUM_PROCESSORS << " \n";
+
+    switch (prty)
+    {
+    case THREAD_PRIORITY_LOWEST:        std::cout << " ----+ priority = THREAD_PRIORITY_LOWEST \n"; break;
+    case THREAD_PRIORITY_BELOW_NORMAL:  std::cout << " ----+ priority = THREAD_PRIORITY_BELOW_NORMAL \n"; break;
+    case THREAD_PRIORITY_NORMAL:        std::cout << " ----+ priority = THREAD_PRIORITY_NORMAL \n"; break;
+    case THREAD_PRIORITY_ABOVE_NORMAL:  std::cout << " ----+ priority = THREAD_PRIORITY_ABOVE_NORMAL \n"; break;
+    case THREAD_PRIORITY_HIGHEST:       std::cout << " ----+ priority = THREAD_PRIORITY_HIGHEST \n"; break;
+    case THREAD_PRIORITY_IDLE:          std::cout << " ----+ priority = THREAD_PRIORITY_IDLE \n"; break;
+    case THREAD_PRIORITY_TIME_CRITICAL: std::cout << " ----+ priority = THREAD_PRIORITY_TIME_CRITICAL \n"; break;
+    default:                            std::cout << " ----+ priority = ? \n"; break;
+    }
+    return;
+}
+
+int main()
+{
+    HANDLE hp = GetCurrentProcess();
+    HANDLE ht = GetCurrentThread();
+    DWORD pid = GetCurrentProcessId();
+    DWORD tid = GetCurrentThreadId();
+    std::cout << " =====================OS05_01=======================\n";
+    std::cout<< "PID = " << pid << ", TID = " << tid << " : " << "\n";//процесс и поток
+    try
+    {
+        {
+            DWORD pa = NULL, sa = NULL;
+            if (!GetProcessAffinityMask(hp, &pa, &sa)) throw "GetProcessAffinityMask";
+            //Функция GetProcessAffinityMask извлекает маску родственного процесса для заданного 
+            //процесса и системную маску родственности для системы
+            std::bitset<8> pa_bits(pa); //числа в двоичном формате (например, bin). Если необходим двоичный вывод 
+            //можно использовать трюк std::bitset:
+            std::cout << " Process Affinity Mask: " << std::showbase << std::hex << pa << "\n";
+            //showbase - Выводить индикатор основания   системы счисления
+            std::cout << " Process Affinity Mask: " << pa_bits << "\n";
+            std::bitset<8> sa_bits(sa);
+           
+            std::cout << " System Affinity Mask: " << std::showbase << sa << "\n";
+            std::cout << " System Affinity Mask: " << sa_bits << "\n";
+            std::cout << " Processor: " <<std::noshowbase<< sa_bits.count() << "\n";
+        }
+    }
+    catch (char* msg) { std::cout << "Error " << msg << "\n"; }
+    std::cout << std::dec << std::noshowbase;
+
+    printProcessPrty(hp);//приоритет процесса
+    printThreadPrty(ht);//приоритет  потока
+    std::cout << "\n\n";
+
+    std::cout << " =====================================================\n";
+    system("pause");
+    return 0;
+}
---
+++ 5 лабораторная работа/os05_02/os05_02/os05_02.cpp
@@ -0,0 +1,120 @@
+﻿// os05_02.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <Windows.h>
+#include <iostream>
+#include <cassert>
+#include <sstream>
+
+using namespace std;
+
+void CreateProccersWithPriority(LPCWSTR an, STARTUPINFO si, int priority)
+{
+    PROCESS_INFORMATION pi;//Структура PROCESS_INFORMATION заполняется функцией CreateProcess с информацией о недавно созданном процессе и его первичном потоке.
+    if (priority == 1)
+    {
+        if (CreateProcess(an,//имя исполняемого модуля
+            NULL, NULL, NULL, FALSE//дескриптор параметра наследования
+            , CREATE_NEW_CONSOLE| IDLE_PRIORITY_CLASS, // флажки создания 
+            NULL, NULL, &si,// информация предустановки
+            &pi// информация о процессе
+        ))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 2)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | BELOW_NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 3)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 4)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | ABOVE_NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 5)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | HIGH_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+    if (priority == 6)
+    {
+        if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | REALTIME_PRIORITY_CLASS, NULL, NULL, &si, &pi))
+            cout << "-- Process OS05_021 created\n";
+        else  cout << "-- Process OS05_021 not created\n";
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "ru");
+
+    for (int i = 0; i < argc; i++)
+    {
+        cout << "arg" << i << ": " << argv[i] << endl;
+    }
+
+    HANDLE hp = GetCurrentProcess();
+
+    try
+    {
+        DWORD pa = NULL, sa = NULL;
+
+        if (!GetProcessAffinityMask(hp, &pa, &sa))
+            throw "GetProcessAffinityMask";
+
+        cout << " Process Affinity Mask: " << showbase << hex << pa << "\n";
+        cout << " System Affinity Mask: " << showbase << hex << sa << "\n";
+        cout << dec << noshowbase << "\n\n";
+
+
+        if (argv[1] != NULL)//целое число, задающее маску доступности процессоров 
+        {
+            unsigned long long mask;
+
+            sscanf_s(argv[1], "%d", &mask);
+
+            if (!SetProcessAffinityMask(hp, mask))
+                throw "SetProcessAffinityMask";
+
+            if (!GetProcessAffinityMask(hp, &pa, &sa))
+                throw "GetProcessAffinityMask";
+
+            cout << " Process Affinity Mask: " << showbase << hex << pa << "\n";
+            cout << " System Affinity Mask: " << showbase << hex << sa << "\n";
+            cout << dec << noshowbase;
+
+            if (argv[2] != NULL && argv[3] != NULL)//целое число, задающее класс приоритета первого, второго дочернего процесса;
+            {
+                STARTUPINFO si;//Структура STARTUPINFO используется функцией CreateProcess для того, чтобы определить оконный терминал, рабочий стол, стандартный дескриптор и внешний вид основного окна для нового процесса.
+
+                ZeroMemory(&si, sizeof(STARTUPINFO));// предназначена для обнуления памяти
+                si.cb = sizeof(STARTUPINFO);
+
+                unsigned long long priority;
+                sscanf_s(argv[2], "%d", &priority);//строка, используемая для извлечения данных формат: строка, содержащая спецификатор(ы) типа
+
+                CreateProccersWithPriority(L"C:\\Users\\thesi\\Desktop\\5\\ОСИС\\Лабораторные_работы\\5 лабораторная работа\\os05_02x\\Debug\\os05_02x.exe", si, priority);
+
+                sscanf_s(argv[3], "%d", &priority);
+
+                ZeroMemory(&si, sizeof(STARTUPINFO));// предназначена для обнуления памяти
+                si.cb = sizeof(STARTUPINFO);
+
+                CreateProccersWithPriority(L"C:\\Users\\thesi\\Desktop\\5\\ОСИС\\Лабораторные_работы\\5 лабораторная работа\\os05_02x\\Debug\\os05_02x.exe", si, priority);
+            }
+        }
+    }
+    catch (char* msg)
+    {
+        cout << "Error " << msg << "\n";
+    }
+}
