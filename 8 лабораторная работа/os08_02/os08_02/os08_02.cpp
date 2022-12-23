0e1339bd3ced4b848efd7fc5a513bee9659a402a
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2022-12-23 11:56:25 +0300

    Add files via upload

---
+++ 8 лабораторная работа/os08_02/os08_02/os08_02.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    clock_t t1 = clock();// возвращает время, пройденное с момента запуска программы вызвавшей функцию clock()
+    while (true)
+    {
+        i++;
+        clock_t t2 = clock();
+        if ((t2 - t1) == 5000 || (t2 - t1) == 10000)
+        {
+            cout << "number: " << i << endl;
+        }
+        if ((t2 - t1) == 15000) break;
+    }
+    cout << "end: " << i << endl;
+}
