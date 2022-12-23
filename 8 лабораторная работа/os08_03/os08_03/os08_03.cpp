b60e480f159332e33726ff972d2040419419d1fc
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2022-12-23 11:58:06 +0300

    Add files via upload

---
+++ 8 лабораторная работа/os08_03/os08_03/os08_03.cpp
@@ -0,0 +1,43 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <ctime>
+#define SECOND 10000000
+
+using namespace std;
+
+int i = 0;
+
+DWORD WINAPI ChildThread()
+{
+    clock_t t1 = clock();
+    while (true)
+    {
+        clock_t t2 = clock();
+        if ((t2 - t1) % 3000 == 0)
+        {
+
+            cout << "number after 3sec.: " << i << endl;
+        }
+        if ((t2 - t1) >= 15000)
+        {
+            cout << "end: " << i << endl;
+            ExitProcess(0);
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    long long it = -0 * SECOND;
+    DWORD ChildId = 0;
+    HANDLE hChild = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChildThread, NULL, NULL, &ChildId);
+    HANDLE timer = CreateWaitableTimer(NULL, FALSE, NULL);//функция создает ожидающий таймер в занятом состоянии
+    if (!SetWaitableTimer(timer, (LARGE_INTEGER*)&it, 3000, NULL, NULL, FALSE)) throw "Error SetWaitableTimer";//Активирует указанный таймер ожидания.
+    clock_t t1 = clock();
+    while (true)
+    {
+        WaitForSingleObject(timer, INFINITE);
+        i++;
+    }
+}
---
+++ 8 лабораторная работа/os08_04/os08_04/os08_04.cpp
@@ -0,0 +1,54 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <ctime>
+#define SECOND 10000000;
+
+using namespace std;
+
+LPCWSTR an = L"C:\\Users\\thesi\\Desktop\\5\\ОСИС\\Лабораторные_работы\\8 лабораторная работа\\os08_o4x\\Debug\\os08_o4x.exe";
+
+DWORD WINAPI ChildThreadProcess1()
+{
+    STARTUPINFO si1;
+    PROCESS_INFORMATION pi1;
+    ZeroMemory(&si1, sizeof(STARTUPINFO));
+    si1.cb = sizeof(STARTUPINFO);
+
+    if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si1, &pi1))
+        cout << endl << "Process 1 created" << endl;
+    else cout << endl << "Process 1 not created" << endl;
+
+    WaitForSingleObject(pi1.hProcess, 5000);
+    TerminateProcess(pi1.hProcess, 0);//завершает работу заданного процесса и всех его потоков.
+    cout << "Process 1 ended" << endl;
+    return 0;
+}
+
+DWORD WINAPI ChildThreadProcess2()
+{
+    STARTUPINFO si2;
+    PROCESS_INFORMATION pi2;
+    ZeroMemory(&si2, sizeof(STARTUPINFO));
+    si2.cb = sizeof(STARTUPINFO);
+
+    if (CreateProcess(an, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si2, &pi2))
+        cout << "Process 2 created" << endl;
+    else cout << endl << "Process 2 not created" << endl;
+
+    WaitForSingleObject(pi2.hProcess, 10000);
+    TerminateProcess(pi2.hProcess, 0);//завершает работу заданного процесса и всех его потоков.
+    cout << "Process 2 ended" << endl;
+
+    return 0;
+}
+
+int main()
+{
+    DWORD ChildId1 = 0;
+    DWORD ChildId2 = 0;
+    HANDLE hChild1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChildThreadProcess1, NULL, NULL, &ChildId1);
+    HANDLE hChild2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChildThreadProcess2, NULL, NULL, &ChildId2);
+
+    WaitForSingleObject(hChild1, INFINITE);
+    WaitForSingleObject(hChild2, INFINITE);
+}
